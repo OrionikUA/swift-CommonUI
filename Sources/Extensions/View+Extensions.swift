4ecd49622330e57be8fa35309c0561071c5467f5
Ihor Popkov <ihor.popkov@gmail.com>
2024-04-14 13:39:14 +0300

    Not Implemented view

--- Sources/Extensions/View+Extensions.swift
+++ Sources/Extensions/View+Extensions.swift
@@ -2,6 +2,11 @@ import Foundation
 import SwiftUI
 
 public extension View {
+    
+    var typeName: String {
+        return String(describing: type(of: self))
+    }
+    
     @ViewBuilder
     func defaultMacOsConfig() -> some View {
         self
---
+++ Sources/Views/NotImplementedView.swift
@@ -0,0 +1,9 @@
+import SwiftUI
+
+public struct NotImplementedView: View {
+    let viewName: String
+    
+    public var body: some View {
+        Text("Not Implemented \(viewName)")
+    }
+}
