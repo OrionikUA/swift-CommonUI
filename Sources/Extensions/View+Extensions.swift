290dd842a7b188e9b0130b07e6a588e658a39753
Ihor Popkov <ihor.popkov@gmail.com>
2024-04-14 19:20:59 +0300

    conditional Background and Foreground

--- Sources/Extensions/View+Extensions.swift
+++ Sources/Extensions/View+Extensions.swift
@@ -13,4 +13,40 @@ public extension View {
             .frame(minWidth: 1460, maxWidth: .infinity, minHeight: 890, maxHeight: .infinity, alignment: .center)
             .preferredColorScheme(.dark)
     }
+    
+    // Foreground
+    
+    func conditionalForegroud(_ value: Color?) -> some View {
+        if let color = value {
+            return AnyView(self.foregroundColor(color))
+        } else {
+            return AnyView(self)
+        }
+    }
+    
+    func conditionalForegroud(_ show: Bool, _ value: Color) -> some View {
+        if (show) {
+            return AnyView(self.foregroundColor(value))
+        } else {
+            return AnyView(self)
+        }
+    }
+    
+    // Background
+    
+    func conditionalBackground(_ value: Color?) -> some View {
+        if let color = value {
+            return AnyView(self.background(color))
+        } else {
+            return AnyView(self)
+        }
+    }
+    
+    func conditionalBackground(_ show: Bool, _ value: Color) -> some View {
+        if (show) {
+            return AnyView(self.background(value))
+        } else {
+            return AnyView(self)
+        }
+    }
 }
