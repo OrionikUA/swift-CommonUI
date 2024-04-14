fa2564bec7eece95c9c856d83e9b06b1b3847e35
Ihor Popkov <ihor.popkov@gmail.com>
2024-04-14 13:40:42 +0300

    Not Implemented View

--- Sources/Views/NotImplementedView.swift
+++ Sources/Views/NotImplementedView.swift
@@ -3,6 +3,10 @@ import SwiftUI
 public struct NotImplementedView: View {
     let viewName: String
     
+    public init(viewName: String) {
+        self.viewName = viewName
+    }
+    
     public var body: some View {
         Text("Not Implemented \(viewName)")
     }
